fa95eb753f45adadfe70ab6e6f3fede9b5978e59
Luke Bertram <lukebertram@gmail.com>
2018-01-07 19:14:53 -0800

    add gitignore file and example logic to .js files

---
+++ js/template-interface.js
@@ -0,0 +1,9 @@
+import { TestClass } from './../template-logic.js';
+const testArray = [];
+$(document).ready(function(){
+  $('#test-form').submit(function(event){
+    event.preventDefault();
+    const input = $('#test-input').val();
+    const freshTest = new
+  })
+});
---
+++ js/template-logic.js
@@ -0,0 +1,8 @@
+export class TestClass{
+  constructor(str){
+    this.str = str;
+  }
+  capitalize(){
+    return this.str.toUpperCase();
+  }
+}

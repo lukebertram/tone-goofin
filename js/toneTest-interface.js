8c3f7d5f4a2e32a7757cf68e2b70e81506acaa86
Luke Bertram <lukebertram@gmail.com>
2018-03-01 14:18:35 -0800

    add arpeggio index reset to togglePlayback function

--- js/toneTest-interface.js
+++ js/toneTest-interface.js
@@ -7,20 +7,29 @@ let isPlaying = false;
 const synth = new Tone.Synth();//create a new synth to play back sounds
 
 synth.toMaster();//connect synth to master output so it can be heard
-
+//these can be combined as follows:
+  // const synth = new Tone.Synth().toMaster();
 const pattern = new Tone.Pattern(
   function(time, note) {
     synth.triggerAttackRelease(note, '16n');
   },
-  ['C4', 'Eb4', 'G4', 'A4', 'Bb4', 'A4', 'G4', 'Eb4']
+  [
+    'C3', 'D3', 'E3', 'G3', 'C4', 'D4', 'E4', 'G4',
+    'C5', 'D5', 'E5', 'G5', 'C6', 'D6', 'E6', 'G6', 'C7' ],
+  "upDown"
 );
 
-pattern.start(0);
+pattern.playbackRate = 2;
+
+//set the pattern to start at the beginning of the Transport timeline
+pattern.start('0:0:0');
+Tone.Transport.bpm.value = 180;
 
 const togglePlayback = function() {
   if (isPlaying) {
     console.log('stop playback');
     Tone.Transport.stop();
+    pattern.index = 0;
   } else {
     console.log('start playback');
     Tone.Transport.start();

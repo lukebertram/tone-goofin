74385a810fd984a3a9cc2a4ba221348c79a6b5a0
Luke Bertram <lukebertram@gmail.com>
2018-03-01 11:53:45 -0800

    add functionality to start,stop playback and set bpm of transport

--- js/toneTest-interface.js
+++ js/toneTest-interface.js
@@ -1,10 +1,41 @@
-import { TestClass } from './../js/toneTest-logic.js';
-const testArray = [];
+import Tone from 'tone';
+
+//GLOBAL VARIABLES
+let isPlaying = false;
+
+//SYNTH SETUP
+const synth = new Tone.Synth();//create a new synth to play back sounds
+
+synth.toMaster();//connect synth to master output so it can be heard
+
+const pattern = new Tone.Pattern(
+  function(time, note) {
+    synth.triggerAttackRelease(note, '16n');
+  },
+  ['C4', 'Eb4', 'G4', 'A4', 'Bb4', 'A4', 'G4', 'Eb4']
+);
+
+pattern.start(0);
+
+const togglePlayback = function() {
+  if (isPlaying) {
+    console.log('stop playback');
+    Tone.Transport.stop();
+  } else {
+    console.log('start playback');
+    Tone.Transport.start();
+  }
+  isPlaying = !isPlaying;
+}
+
+
+
+// FRONT-END FOOLERY
 $(document).ready(function(){
-  $('#test-form').submit(function(event){
+  document.getElementById('toggle-play').onclick = togglePlayback;
+  $('#bpm-form').submit(function(event){
     event.preventDefault();
-    const input = $('#test-input').val();
-    const freshTest = new TestClass(input);
-    $('#test-output').append(`<li>${freshTest.capitalize()}</li>`);
+    const newBpm = parseInt($('#bpm-input').val());
+    Tone.Transport.bpm.value = newBpm;
   });
 });

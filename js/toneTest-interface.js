d63fd17ac5ad7fb65c2d07aa1dadb707a517dcbc
Luke Bertram <lukebertram@gmail.com>
2018-03-01 16:42:58 -0800

    beautify the demo arpeggio pattern

--- js/toneTest-interface.js
+++ js/toneTest-interface.js
@@ -9,27 +9,34 @@ const synth = new Tone.Synth();//create a new synth to play back sounds
 synth.toMaster();//connect synth to master output so it can be heard
 //these can be combined as follows:
   // const synth = new Tone.Synth().toMaster();
-const pattern = new Tone.Pattern(
+const prelude = new Tone.Pattern(
   function(time, note) {
     synth.triggerAttackRelease(note, '16n');
   },
   [
     'C3', 'D3', 'E3', 'G3', 'C4', 'D4', 'E4', 'G4',
-    'C5', 'D5', 'E5', 'G5', 'C6', 'D6', 'E6', 'G6', 'C7' ],
-  "upDown"
+    'C5', 'D5', 'E5', 'G5', 'C6', 'D6', 'E6', 'G6',
+    'C7', 'G6', 'E6', 'D6', 'C6', 'G5', 'E5', 'D5',
+    'C5', 'G4', 'E4', 'D4', 'C4', 'G3', 'E3', 'D3',
+
+    'A2', 'B2', 'C3', 'E3', 'A3', 'B3', 'C4', 'E4',
+    'A4', 'B4', 'C5', 'E5', 'A5', 'B5', 'C6', 'E6',
+    'A6', 'E6', 'C6', 'B5', 'A5', 'E5', 'C5', 'B4',
+    'A4', 'E4', 'C4', 'B3', 'A3', 'E3', 'C3', 'B2'
+  ]
 );
 
-pattern.playbackRate = 2;
+prelude.playbackRate = 2;
 
-//set the pattern to start at the beginning of the Transport timeline
-pattern.start('0:0:0');
+//set the prelude pattern to start at the beginning of the Transport timeline
+prelude.start('0:0:0');
 Tone.Transport.bpm.value = 180;
 
 const togglePlayback = function() {
   if (isPlaying) {
     console.log('stop playback');
     Tone.Transport.stop();
-    pattern.index = 0;
+    prelude.index = 0;
   } else {
     console.log('start playback');
     Tone.Transport.start();
